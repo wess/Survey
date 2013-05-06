39ff289803898f008f0f145df3103f915b54f25f
Robin Kunde <robin.kunde@recoursive.com>
2013-05-06 14:53:52 -0400

    fixed bug in email validator

--- Survey/SurveyValidator.m
+++ Survey/SurveyValidator.m
@@ -147,7 +147,7 @@
     if(!_emailAddressBlock)
     {
         _emailAddressBlock = ^BOOL(NSString *value) {
-            NSString *pattern           = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+            NSString *pattern           = @"^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$";
             NSRegularExpression *regex  = [NSRegularExpression regularExpressionWithPattern:pattern options:0 error:nil];
             NSArray *matches            = [regex matchesInString:value options:0 range:NSMakeRange(0, value.length)];
             
